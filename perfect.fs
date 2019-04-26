4e0568c9fe034cb97cffd6a501b03e4fe0b9b39b
ShrikarDatar <47650216+ShrikarDatar@users.noreply.github.com>
2019-04-26 17:12:18 +0530

    Update perfect.fs

--- perfect.fs
+++ perfect.fs
@@ -1,13 +1,13 @@
 open System
-let mutable e=0
-let mutable sum=0
+let mutable n=0
+let mutable count=0
 printfn"Enter range"
-e<-int(Console.ReadLine())
-for i=1 to e do
-    sum<-0
+n<-int(Console.ReadLine())
+for i=1 to n do
+    count<-0
     for j=1 to i do
        if i%j=0 then
-        sum<-sum+j
-        if sum=i then
+        count<-count+j
+        if count=i then
             printfn"%d"i  
 Console.Read()               
