394213f3339966d83f0e5f210840455a863fd2d7
ShrikarDatar <47650216+ShrikarDatar@users.noreply.github.com>
2019-05-03 04:59:41 +0530

    Update prime1-100.fs

--- prime1-100.fs
+++ prime1-100.fs
@@ -13,3 +13,18 @@ for n=1 to 1 do
         printfn "%d "n
    
 Console.Read()
+//Check Prime number till 1 to n 
+let primeN num=
+    let mutable flag=0
+    for i=2 to num do
+        flag<-0
+        for j=2 to i-1 do
+            if i%j=0 then
+                flag<-1
+        if flag=0 && i>=2 then
+            printfn"%d is  prime"i
+        
+printfn"Enter number till where to check for prime  :"
+let n9=int(Console.ReadLine())
+primeN n9
+
