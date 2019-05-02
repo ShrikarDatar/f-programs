591bb963688f7fc6f520e26f10f80bfd752ab50e
ShrikarDatar <47650216+ShrikarDatar@users.noreply.github.com>
2019-05-03 05:00:37 +0530

    Update recursionfact.fs

--- recursionfact.fs
+++ recursionfact.fs
@@ -8,3 +8,13 @@ let rec fact n =
 let res=fact 5
 printfn "%d"res
 Console.ReadLine()
+//Check factorial of number 
+let factorial num=
+    let mutable fact=1
+    for i=1 to num do
+        fact<-fact*i
+    printfn"%d factorial = %d"num fact    
+
+printfn"Enter a number to calculate its factorial  :"
+let n1=int(Console.ReadLine())
+factorial n1
